8a67c6d1f5bda2b38211f689ef5c0ad4de021a4f
Derek Haynes <dhaynes@redhat.com>
2014-05-21 15:28:20 -0400

    Pull out Javascript into common file Enable drop downs calling services on view roles/summary pages

--- apps-common/src/main/webapp/js/lightblue-apps.js
+++ apps-common/src/main/webapp/js/lightblue-apps.js
@@ -1,124 +1,43 @@
-		(function( $ ) {
-			$.widget( "custom.combobox", {
-				_create: function() {
-					this.wrapper = $( "<span>" )
-					.addClass( "custom-combobox" )
-					.insertAfter( this.element );
-					this.element.hide();
-					this._createAutocomplete();
-					this._createShowAllButton();
-				},
-				_createAutocomplete: function() {
-					var selected = this.element.children( ":selected" ),
-					value = selected.val() ? selected.text() : "";
-					this.input = $( "<input>" )
-					.appendTo( this.wrapper )
-					.val( value )
-					.attr( "title", "" )
-					.addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
-					.autocomplete({
-						delay: 0,
-						minLength: 0,
-						source: $.proxy( this, "_source" )
-					})
-					.tooltip({
-						tooltipClass: "ui-state-highlight"
-					});
-					this._on( this.input, {
-						autocompleteselect: function( event, ui ) {
-							ui.item.option.selected = true;
-							this._trigger( "select", event, {
-								item: ui.item.option
-							});
-						},
-						autocompletechange: "_removeIfInvalid"
-					});
-				},
-				_createShowAllButton: function() {
-					var input = this.input,
-					wasOpen = false;
-					$( "<a>" )
-					.attr( "tabIndex", -1 )
-					.attr( "title", "Show All Items" )
-					.tooltip()
-					.appendTo( this.wrapper )
-					.button({
-						icons: {
-							primary: "ui-icon-triangle-1-s"
-						},
-						text: false
-					})
-					.removeClass( "ui-corner-all" )
-					.addClass( "custom-combobox-toggle ui-corner-right" )
-					.mousedown(function() {
-						wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-					})
-					.click(function() {
-						input.focus();
-		// Close if already visible
-		if ( wasOpen ) {
-			return;
-		}
-		// Pass empty string as value to search for, displaying all results
-		input.autocomplete( "search", "" );
-	});
-				},
-				_source: function( request, response ) {
-					var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-					response( this.element.children( "option" ).map(function() {
-						var text = $( this ).text();
-						if ( this.value && ( !request.term || matcher.test(text) ) )
-							return {
-								label: text,
-								value: text,
-								option: this
-							};
-						}) );
-				},
-				_removeIfInvalid: function( event, ui ) {
-		// Selected an item, nothing to do
-		if ( ui.item ) {
-			return;
-		}
-		// Search for a match (case-insensitive)
-		var value = this.input.val(),
-		valueLowerCase = value.toLowerCase(),
-		valid = false;
-		this.element.children( "option" ).each(function() {
-			if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-				this.selected = valid = true;
-				return false;
-			}
-		});
-		// Found a match, nothing to do
-		if ( valid ) {
-			return;
-		}
-		// Remove invalid value
-		this.input
-		.val( "" )
-		.attr( "title", value + " didn't match any item" )
-		.tooltip( "open" );
-		this.element.val( "" );
-		this._delay(function() {
-			this.input.tooltip( "close" ).attr( "title", "" );
-		}, 2500 );
-		this.input.data( "ui-autocomplete" ).term = "";
-	},
-	_destroy: function() {
-		this.wrapper.remove();
-		this.element.show();
-	}
-});
-	})( jQuery );
-	$(function() {
-		$( "#combobox, #combobox2, #combobox3" ).combobox();
-		$( "#toggle" ).click(function() {
-			$( "#combobox, #combobox2, #combobox3" ).toggle();
-		});
-	});
+  $(document).ready(function() {
 
-	$(function() {
-		$( "#accordion" ).accordion();
-	});
-	
+	  var metadataServicePath = "http://localhost:8080/metadata/";
+	  
+	  var entitySelect = $("#entities");
+	  var versionSelect = $("#versions");
+	  var submitButton = $("#load-content-btn");
+
+	  var jsonTextArea = $("#json");
+	  var jsonTreeEditor = $("#editor");
+	  
+      $.getJSON( metadataServicePath, function( json ) {
+	      $.each( json, function( key, val ) {
+	    	  $.each( val, function( arrayVal ) {
+	    		  entitySelect.append("<option value='" + val[arrayVal] + "'>" + val[arrayVal] + "</option>");
+	          });   	  
+	      });
+       });
+
+      entitySelect.change(function() {
+    	  versionSelect.empty();
+    	  versionSelect.append("<option value='' disabled selected>Version:</option>");
+          $.getJSON( metadataServicePath + entitySelect.val(), function( json ) {
+              $.each( json, function( versions, details ) {
+                  $.each(details, function( arrayIndex ) {
+                 	  $.each(details[arrayIndex], function(attributeName, value) {
+                 		   if(attributeName == "value") {
+                 			  versionSelect.append("<option value='" + value + "'>" + value + "</option>");
+                 		   }                		    
+                 	  });
+                  });         
+              });
+           });
+      });
+
+      submitButton.click(function() {
+        $.getJSON( metadataServicePath + entitySelect.val() + "/" + versionSelect.val(), function( json ) {
+        	jsonTextArea.val(JSON.stringify(json));
+        	jsonTreeEditor.jsonEditor(json, { change: updateJSON, propertyclick: showPath });
+        });
+      });
+           
+  });
